7c0743588446d956413ca06a86116780d0587a7c
ning_global <ning_global@javakc.com>
2021-04-06 14:27:09 +0800

    分支提交

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,6 +1,9 @@
 public class A {
 
     private String ning;
+    private String ning1;
+    private String ning2;
+    private String ning3;
 
     public static void main(String[] args) {
         System.out.println(111);
